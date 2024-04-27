b6413a897c463a860dad8ddf07862597d057ef13
KJChoi <kjchoi@surromind.ai>
2024-04-27 10:25:11 +0900

    풍선 터트리기 기본 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import {
+		crateGrid,
+		getAllConnectedBalloons,
+		getMaxSizeOfConnectedBalloons,
+		setConnectedBalloonsToFalse,
+} from "./utils";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+		const [grid, setGrid] = useState<boolean[][]>(crateGrid(5));
+		const allConnectedBalloons = getAllConnectedBalloons(grid);
+
+		const handleBalloonClick = (rowIndex: number, cellIndex: number) => {
+				const currentBalloon = allConnectedBalloons[rowIndex][cellIndex];
+				const maxSize = getMaxSizeOfConnectedBalloons(allConnectedBalloons);
+
+				if (maxSize > currentBalloon.size) {
+						alert("터뜨릴 수 없습니다.");
+				} else {
+						popBalloonAndCheckEndGame(currentBalloon);
+				}
+		};
+
+		const popBalloonAndCheckEndGame = (currentBalloon: { size: number, coors: number[][] }) => {
+				const newGrid = setConnectedBalloonsToFalse(grid, currentBalloon.coors);
+				setGrid(newGrid);
+
+				const remainingBalloons = getAllConnectedBalloons(newGrid);
+				const remainingMaxSize = getMaxSizeOfConnectedBalloons(remainingBalloons);
+
+				if (remainingMaxSize === 0) {
+						endGame();
+				}
+		};
+
+		const endGame = () => {
+				alert("게임 끝");
+				setGrid(crateGrid(5));
+		};
+
+		return (
+			<div className={"grid"}>
+					{grid.map((row, rowIndex) => (
+						<div key={rowIndex} className={"row"}>
+								{row.map((cell, cellIndex) => (
+									<div key={cellIndex} className={"cell"}>
+											<button onClick={() => handleBalloonClick(rowIndex, cellIndex)}>
+													{cell ? "🎈" : ""}
+											</button>
+									</div>
+								))}
+						</div>
+					))}
+			</div>
+		);
 }
 
 export default App;
