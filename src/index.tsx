f786f74ab28297756714dca6db32d2979208cbf6
KJChoi <kjchoi@surromind.ai>
2024-04-28 16:09:40 +0900

    react query 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Game from './Game';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+	<BrowserRouter>
+			<Routes>
+					<Route path="/" element={<Game />}></Route>
+			</Routes>
+	</BrowserRouter>
 );
